20050c2d9b1dd486def3237642b591c9ed7c14f8
SIMSON20 <desiredosseh20@gmail.com>
2022-12-10 10:00:34 +0100

    new file:   __pycache__/compute_stats_refactor.cpython-311.pyc 	new file:   __pycache__/unittest.cpython-311.pyc 	modified:   compute_stats_refactor.py 	new file:   unittest.py

--- compute_stats_refactor.py
+++ compute_stats_refactor.py
@@ -1,4 +1,3 @@
-import unittest
 import math
 
 def compute_stats():
@@ -79,32 +78,8 @@ def standard_dev(data=None):
     return math.sqrt(sum([(x-avg)**2 for x in data])/len(data))
 
 
-class Test_Additional_Stats(unittest.TestCase):
-    def test_read_ints(self):
-        self.assertNotEqual(len(read_ints()), 0)
 
-    def test_count_numbers(self):
-        self.assertEqual(count_numbers([2, 3, 9]), 3)
-
-    def test_summation(self):
-        self.assertEqual(summation([1, 2, 3, 4, 5]), 15)
-
-    def test_average(self):
-        self.assertEqual(average([1, 2, 3, 4, 5]), 3)
-
-    def test_maximum(self):
-        self.assertEqual(maximum([5, 8, 9]), 9)
-
-    def test_harmonic_mean(self):
-        self.assertEqual(harmonic_mean([4, 1]), 8/5)
-
-    def test_variance(self):
-        self.assertEqual(variance([2, 4]), 1)
-
-    def test_standard_dev(self):
-        self.assertEqual(standard_dev([2, 4]), 1)
 
 
 if __name__ == '__main__':
     compute_stats()
-    unittest.main()
